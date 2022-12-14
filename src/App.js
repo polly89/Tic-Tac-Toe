1d707bb9abbb158237004db9a697822e5d4ebb08
Christina Woodhams <cwoodhams89@gmail.com>
2022-12-13 20:55:17 -0500

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
+import Square from './Square';
 import './App.css';
 
 function App() {
+  const pretty = 'Abby'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Square propVar={pretty}/>
     </div>
   );
 }
---
+++ src/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Square = (props) => {
+    return (
+    <div className="square">
+        {props.pretty}
+    </div>
+    );
+};
+
+export default Square;
