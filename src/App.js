bcdeb3d244485bdd37c0d3dd4f4c32d642395b2d
Christina Woodhams <cwoodhams89@gmail.com>
2022-12-16 07:56:00 -0500

    Initialized state variables for the game board and the player turn. Passed state variables and setters as props to the square component. Verified props are being passed correctly.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import Square from './Square';
 import './App.css';
 
-function App() {
-  const pretty = 'Abby'
+const App = (props) => {
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
 
   return (
     <div className="App">
-      <Square propVar={pretty}/>
+      <Square
+      squares={squares}
+      setSquares={setSquares}
+      player={player}
+      setPlayer={setPlayer}
+    />
     </div>
   );
 }
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Square = (props) => {
+    console.log(props.squares, props.player)
     return (
     <div className="square">
-        {props.pretty}
+        
     </div>
     );
 };
